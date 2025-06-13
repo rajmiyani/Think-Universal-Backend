d9ae9b1d9c7a6399e0aeabc3f01e126572ba8ef9
rajmiyani <rajmiyani06@gmail.com>
2025-06-13 14:53:19 +0530

    rf

--- Backend/router/report.router.js
+++ Backend/router/report.router.js
@@ -11,7 +11,7 @@ import upload from '../multer.js';
 const router = express.Router();
 
 // GET /reports/getReports
-router.get('/getReports', validate(reportFilterSchema, 'query'), getReports);
+router.get('/getReports', getReports);
 
 // GET /reports/export
 router.get('/export', validate(reportFilterSchema, 'query'), exportCSV);
