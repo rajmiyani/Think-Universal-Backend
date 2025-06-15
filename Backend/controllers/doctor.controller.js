26439fb914c6b7b3f049344e73a74471c65c759e
Raj Miyani <166621455+rajmiyani@users.noreply.github.com>
2025-06-15 12:55:51 +0530

    Update doctor.controller.js

--- Backend/controllers/doctor.controller.js
+++ Backend/controllers/doctor.controller.js
@@ -57,3 +57,19 @@ export const addDoctor = async (req, res) => {
 
     }
 };
+
+export const addDoctor = async (req, res) => {
+    try {
+        const data = await Doctor.find();
+         return res.status(201).json({
+            success: true,
+            message: "Doctor find",
+            data
+        });
+     } catch (err) {
+        console.error("Add Doctor Error:", err);
+        return res.status(500).json({
+            success: false, message: "Server error", error: err.message, stack: err.stack
+        });
+
+    }
