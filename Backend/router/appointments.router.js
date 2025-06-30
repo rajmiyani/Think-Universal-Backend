11467e22d0a73d0ed1a1c55af131f15ca2f1d5e7
rajmiyani <rajmiyani06@gmail.com>
2025-06-30 16:44:22 +0530

    rf

--- Backend/router/appointments.router.js
+++ Backend/router/appointments.router.js
@@ -1,10 +1,6 @@
 import express from 'express';
 import {
   getAppointments,
-  // getAppointmentsByDoctor,
-  // createAppointment,
-  // updateAppointment,
-  // deleteAppointment,
   exportCSV,
 } from '../controllers/appointment.controller.js';
 
@@ -13,10 +9,6 @@ import { verifyToken, authorizeRoles } from '../middleware/admin.middlware.js';
 const router = express.Router();
 
 router.get("/getAppointments", getAppointments); // For admin panel
-// router.get("/doctor/:doctorId", verifyToken, authorizeRoles('doctor', 'admin'), getAppointmentsByDoctor);
-// router.post('/createAppointment', verifyToken, authorizeRoles('patient', 'admin'), createAppointment);
-// router.put('/updateAppointment/:id', verifyToken, authorizeRoles('admin', 'doctor'), updateAppointment);
-// router.delete('/deleteAppointment/:id', verifyToken, authorizeRoles('admin'), deleteAppointment);
 router.get("/export/csv", exportCSV);
 
 export default router;
